a797ab670ce1f230aa2e4acee88142c97ac11cd6
eballantine <lizzy.kayballantine@gmail.com>
2021-09-30 16:15:12 +0100

    Refactor backstage quality calculation

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,5 +1,4 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
@@ -14,12 +13,8 @@ class GildedRose
   private
 
   def update_quality(item)
-      if item.name == 'Conjured' 
-        decrease_quality(item)
-        decrease_quality(item)
-      elsif item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
         increase_quality(item)
-        backstage_bonus(item) if item.name == "Backstage passes to a TAFKAL80ETC concert"
       else
         decrease_quality(item) 
       end 
@@ -36,21 +31,23 @@ class GildedRose
     end
   end
 
-  def backstage_bonus(item)
-    if item.sell_in < 11 
-      increase_quality(item)
-    end
-    if item.sell_in < 6 
-      increase_quality(item)
-    end
+  def backstage_quality(item)
+    item.quality += 1 if item.quality < 50 
+    item.quality += 1 if item.sell_in < 11 && item.quality < 50 
+    item.quality += 1 if item.sell_in < 6 && item.quality < 50 
   end
 
   def increase_quality(item)
-    item.quality += 1 unless item.quality == 50 
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      backstage_quality(item)
+    else
+      item.quality += 1 unless item.quality == 50 
+    end
   end
 
   def decrease_quality(item)
     item.quality -= 1 unless item.quality == 0 || item.name == "Sulfuras, Hand of Ragnaros"
+    item.quality -= 1 if item.name == 'Conjured' 
   end
 
   def decrease_sell_in(item)
