0712c0d9f817eec695f4bc3c0950a192addc2a89
eballantine <lizzy.kayballantine@gmail.com>
2021-09-30 15:27:07 +0100

    Refactor sell in date has passed

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -21,21 +21,21 @@ class GildedRose
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
         if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
+          item.quality -= 1
         end
       end
     else
       if item.quality < 50
-        item.quality = item.quality + 1
+        item.quality += 1
         if item.name == "Backstage passes to a TAFKAL80ETC concert"
           if item.sell_in < 11
             if item.quality < 50
-              item.quality = item.quality + 1
+              item.quality += 1
             end
           end
           if item.sell_in < 6
             if item.quality < 50
-              item.quality = item.quality + 1
+              item.quality += 1
             end
           end
         end
@@ -45,20 +45,14 @@ class GildedRose
   end
 
   def sell_in_passed(item)
-    if item.name != "Aged Brie"
-      if item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        item.quality = item.quality - item.quality
-      end
-    else
-      if item.quality < 50
-        item.quality = item.quality + 1
-      end
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      item.quality = 0 
+    elsif item.name == "Sulfuras, Hand of Ragnaros"
+      item.quality = 80
+    elsif item.name == "Aged Brie" && item.quality < 50
+      item.quality += 1
+    elsif item.quality > 0
+      item.quality -= 1
     end
   end
 end
