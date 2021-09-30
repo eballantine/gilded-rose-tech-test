9d242eccb7bb23f8c8b6531ba2292fe9fc274454
eballantine <lizzy.kayballantine@gmail.com>
2021-09-30 14:43:54 +0100

    Test for normal item

---
+++ ruby/spec/features/normal_item_spec.rb
@@ -0,0 +1,20 @@
+require 'gilded_rose'
+
+describe 'Normal Item' do
+
+  context 'sell_in > 0' do
+    bread = Item.new('Bread', 10, 10)
+    let(:items) { [bread] }
+  
+    it 'should decrease in value by 1 each day' do
+      gilded_rose = GildedRose.new(items)
+      gilded_rose.update_quality()
+      expect(items[0].quality).to eq 9
+      expect(items[0].sell_in).to eq 9
+    end
+
+    it 'should decrease in sell_in by 1 each day' do
+      expect(items[0].sell_in).to eq 9
+    end
+  end
+end
--- ruby/spec/features/sulfuras_spec.rb
+++ ruby/spec/features/sulfuras_spec.rb
@@ -1,14 +1,12 @@
 require 'gilded_rose'
 
 describe 'Sulfuras' do
-  context 'sell_in > 0' do
-    sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 0, 80)
-    let(:items) { [sulfuras] }
-  
-    it 'should always have a quality value of 80' do
-      gilded_rose = GildedRose.new(items)
-      gilded_rose.update_quality()
-      expect(items[0].quality).to eq 80
-    end
+  sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 0, 80)
+  let(:items) { [sulfuras] }
+
+  it 'should always have a quality value of 80' do
+    gilded_rose = GildedRose.new(items)
+    gilded_rose.update_quality()
+    expect(items[0].quality).to eq 80
   end
 end
