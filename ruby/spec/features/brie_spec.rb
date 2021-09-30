01211bb1e97f3286a90f734a4284f2c656543e7f
eballantine <lizzy.kayballantine@gmail.com>
2021-09-30 14:47:08 +0100

    Tests for normal item and normal 'rules'
    Added a test to brie to ensure value never goes over 50 - the only item with a mechanism that might allow this

--- ruby/spec/features/brie_spec.rb
+++ ruby/spec/features/brie_spec.rb
@@ -1,7 +1,18 @@
 require 'gilded_rose'
 
 describe 'Aged Brie' do
-
+  
+  context 'value = 50' do
+    brie = Item.new('Aged Brie', 10, 50)
+    let(:items) { [brie] }
+  
+    it 'should never have a value over 50' do
+      gilded_rose = GildedRose.new(items)
+      gilded_rose.update_quality()
+      expect(items[0].quality).to eq 50
+    end
+  end
+  
   context 'sell_in > 0' do
     brie = Item.new('Aged Brie', 20, 10)
     let(:items) { [brie] }
--- ruby/spec/features/normal_item_spec.rb
+++ ruby/spec/features/normal_item_spec.rb
@@ -17,4 +17,15 @@ describe 'Normal Item' do
       expect(items[0].sell_in).to eq 9
     end
   end
+
+  context 'value = 0' do
+    bread = Item.new('Bread', 1, 0)
+    let(:items) { [bread] }
+  
+    it 'should never have a value less than 0' do
+      gilded_rose = GildedRose.new(items)
+      gilded_rose.update_quality()
+      expect(items[0].quality).to eq 0
+    end
+  end
 end
